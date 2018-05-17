e7fe84c45a245595402e2a1325db1331689610c3
Chen Yu Pao <windperson@gmail.com>
2018-05-17 15:26:28 +0800

    comlete Add a model

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -5,7 +5,7 @@ namespace MvcMovie.Models
 {
     public class Movie
     {
-         public int ID { get; set; }
+        public int ID { get; set; }
         public string Title { get; set; }
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
